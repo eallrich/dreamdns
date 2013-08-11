fb332d12635554d8fcaa9428832f3b434920eade
Evan Allrich <evan@unguku.com>
2013-08-11 13:13:09 -0500

    Try declaring the dependency on requests

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 
 setup(
     name='dreamdns',
     version='0.1',
     packages=['dreamdns',],
+    install_requires=['requests == 1.2.3',],
     license=open('LICENSE').read(),
     long_description=open('README.md').read(),
 )
