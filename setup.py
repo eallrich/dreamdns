666993b3c4174f215fd046342b028c0c7494b58b
Evan Allrich <evan@unguku.com>
2013-08-11 13:07:26 -0500

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='dreamdns',
+    version='0.1',
+    packages=['dreamdns',],
+    license=open('LICENSE').read(),
+    long_description=open('README.md').read(),
+)
